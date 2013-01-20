bebe4069bdfc5420053126f10cfc3ef0e5d28b5f
Trask Stalnaker <trask.stalnaker@gmail.com>
2013-01-20 02:22:35 -0800

    Minor type correction
    Wasn't causing an issue, but was incorrect nonetheless.

--- core/src/main/java/io/informant/core/util/DaemonExecutors.java
+++ core/src/main/java/io/informant/core/util/DaemonExecutors.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011-2012 the original author or authors.
+ * Copyright 2011-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -81,12 +81,12 @@ public final class DaemonExecutors {
 
     private static ExecutorService newExecutorServiceProxy(ThreadPoolExecutor executor) {
         return (ExecutorService) Proxy.newProxyInstance(executor.getClass()
-                .getClassLoader(), new Class<?>[] { ScheduledExecutorService.class },
+                .getClassLoader(), new Class<?>[] { ExecutorService.class },
                 new ExecutorInvocationHandler(executor));
     }
 
     private static ScheduledExecutorService newScheduledExecutorServiceProxy(
-            ThreadPoolExecutor executor) {
+            ScheduledThreadPoolExecutor executor) {
         return (ScheduledExecutorService) Proxy.newProxyInstance(executor.getClass()
                 .getClassLoader(), new Class<?>[] { ScheduledExecutorService.class },
                 new ExecutorInvocationHandler(executor));
