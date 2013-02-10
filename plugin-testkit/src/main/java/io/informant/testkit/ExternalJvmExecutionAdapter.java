d1e8731167f4edc94d34cf3e1e9689655f24d1fa
Trask Stalnaker <trask.stalnaker@gmail.com>
2013-02-09 22:09:49 -0800

    Shutdown hook to propagate kill to external jvm
    Unfortunately, ctrl-c during maven test will kill the maven process, but
    won't kill the forked surefire jvm where the tests are being run
    (http://jira.codehaus.org/browse/SUREFIRE-413), and so this shutdown hook
    won't get triggered by ctrl-c while running tests under maven.

--- plugin-testkit/src/main/java/io/informant/testkit/ExternalJvmExecutionAdapter.java
+++ plugin-testkit/src/main/java/io/informant/testkit/ExternalJvmExecutionAdapter.java
@@ -50,39 +50,12 @@ class ExternalJvmExecutionAdapter implements ExecutionAdapter {
 
     private final Process process;
     private final ExecutorService consolePipeExecutorService;
-
     private final SocketCommander socketCommander;
+    private final Thread shutdownHook;
 
-    ExternalJvmExecutionAdapter(Map<String, String> properties) throws IOException {
-        List<String> command = Lists.newArrayList();
-        String javaExecutable = System.getProperty("java.home") + File.separator + "bin"
-                + File.separator + "java";
-        command.add(javaExecutable);
-        String classpath = System.getProperty("java.class.path");
-        command.add("-cp");
-        command.add(classpath);
-        RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
-        List<String> arguments = runtimeMXBean.getInputArguments();
-        for (String argument : arguments) {
-            if (argument.startsWith("-javaagent:")) {
-                // pass on the jacoco agent in particular
-                command.add(argument);
-            }
-        }
-        File javaagentJarFile = ClassPath.getInformantCoreJarFile();
-        if (javaagentJarFile == null) {
-            javaagentJarFile = ClassPath.getDelegatingJavaagentJarFile();
-            command.add("-javaagent:" + javaagentJarFile);
-            command.add("-DdelegateJavaagent=" + MainEntryPoint.class.getName());
-        } else {
-            command.add("-javaagent:" + javaagentJarFile);
-        }
-        for (Entry<String, String> agentProperty : properties.entrySet()) {
-            command.add("-Dinformant." + agentProperty.getKey() + "=" + agentProperty.getValue());
-        }
-        command.add(ExternalJvmExecutionAdapter.class.getName());
+    ExternalJvmExecutionAdapter(final Map<String, String> properties) throws IOException {
         socketCommander = new SocketCommander();
-        command.add(Integer.toString(socketCommander.getLocalPort()));
+        List<String> command = buildCommand(properties, socketCommander.getLocalPort());
         ProcessBuilder processBuilder = new ProcessBuilder(command);
         processBuilder.redirectErrorStream(true);
         process = processBuilder.start();
@@ -96,6 +69,23 @@ class ExternalJvmExecutionAdapter implements ExecutionAdapter {
                 }
             }
         });
+        shutdownHook = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    socketCommander.sendKillCommand();
+                } catch (IOException e) {
+                    logger.error(e.getMessage(), e);
+                } catch (InterruptedException e) {
+                    logger.error(e.getMessage(), e);
+                }
+            }
+        };
+        // unfortunately, ctrl-c during maven test will kill the maven process, but won't kill the
+        // forked surefire jvm where the tests are being run
+        // (http://jira.codehaus.org/browse/SUREFIRE-413), and so this hook won't get triggered by
+        // ctrl-c while running tests under maven
+        Runtime.getRuntime().addShutdownHook(shutdownHook);
     }
 
     public int getPort() throws IOException, InterruptedException {
@@ -104,7 +94,6 @@ class ExternalJvmExecutionAdapter implements ExecutionAdapter {
 
     public void executeAppUnderTest(Class<? extends AppUnderTest> appUnderTestClass,
             String threadName) throws IOException, InterruptedException {
-
         socketCommander.sendCommand(ImmutableList.of(SocketCommandProcessor.EXECUTE_APP_COMMAND,
                 appUnderTestClass.getName(), threadName));
     }
@@ -117,6 +106,7 @@ class ExternalJvmExecutionAdapter implements ExecutionAdapter {
         socketCommander.close();
         process.waitFor();
         consolePipeExecutorService.shutdownNow();
+        Runtime.getRuntime().removeShutdownHook(shutdownHook);
     }
 
     public void kill() throws IOException, InterruptedException {
@@ -124,6 +114,7 @@ class ExternalJvmExecutionAdapter implements ExecutionAdapter {
         socketCommander.close();
         process.waitFor();
         consolePipeExecutorService.shutdownNow();
+        Runtime.getRuntime().removeShutdownHook(shutdownHook);
     }
 
     public static void main(String[] args) {
@@ -138,5 +129,44 @@ class ExternalJvmExecutionAdapter implements ExecutionAdapter {
             // log error and exit gracefully
             logger.error(t.getMessage(), t);
         }
+    }
+
+    private static List<String> buildCommand(Map<String, String> properties, int port)
+            throws IOException {
+        List<String> command = Lists.newArrayList();
+        String javaExecutable = System.getProperty("java.home") + File.separator + "bin"
+                + File.separator + "java";
+        command.add(javaExecutable);
+        String classpath = System.getProperty("java.class.path");
+        command.add("-cp");
+        command.add(classpath);
+        command.addAll(getJavaAgentsFromCurrentJvm());
+        File javaagentJarFile = ClassPath.getInformantCoreJarFile();
+        if (javaagentJarFile == null) {
+            javaagentJarFile = ClassPath.getDelegatingJavaagentJarFile();
+            command.add("-javaagent:" + javaagentJarFile);
+            command.add("-DdelegateJavaagent=" + MainEntryPoint.class.getName());
+        } else {
+            command.add("-javaagent:" + javaagentJarFile);
+        }
+        for (Entry<String, String> agentProperty : properties.entrySet()) {
+            command.add("-Dinformant." + agentProperty.getKey() + "=" + agentProperty.getValue());
+        }
+        command.add(ExternalJvmExecutionAdapter.class.getName());
+        command.add(Integer.toString(port));
+        return command;
+    }
+
+    private static List<String> getJavaAgentsFromCurrentJvm() {
+        RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
+        List<String> arguments = runtimeMXBean.getInputArguments();
+        List<String> javaAgents = Lists.newArrayList();
+        for (String argument : arguments) {
+            if (argument.startsWith("-javaagent:")) {
+                // pass on the jacoco agent in particular
+                javaAgents.add(argument);
+            }
+        }
+        return javaAgents;
     }
 }
