98b02e58d80cd73570621834e48d2aea1d6054c6
Trask Stalnaker <trask.stalnaker@gmail.com>
2014-01-27 23:43:37 -0800

    Re-implement password hashing

--- core/src/main/java/org/glowroot/config/PasswordHash.java
+++ core/src/main/java/org/glowroot/config/PasswordHash.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 the original author or authors.
+ * Copyright 2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,112 +15,66 @@
  */
 package org.glowroot.config;
 
-import java.math.BigInteger;
-import java.security.NoSuchAlgorithmException;
+import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 
+import com.google.common.io.BaseEncoding;
+
 import org.glowroot.markers.Static;
 
-/*
- * PBKDF2 salted password hashing from http://crackstation.net/hashing-security.htm
- * 
- * @author havoc AT defuse.ca
+/**
+ * @author Trask Stalnaker
  * @since 0.5
  */
-// would be nice to use jasypt, but it requires very large ICU dependency on JDK5
 @Static
 class PasswordHash {
 
-    private static final String PBKDF2_ALGORITHM = "PBKDF2WithHmacSHA1";
+    private static final int ITERATION_COUNT = 100000;
 
-    // The following constants may be changed without breaking existing hashes
-    private static final int SALT_BYTE_SIZE = 24;
-    private static final int HASH_BYTE_SIZE = 24;
-    private static final int PBKDF2_ITERATIONS = 1000;
-
-    private static final int ITERATION_INDEX = 0;
-    private static final int SALT_INDEX = 1;
-    private static final int PBKDF2_INDEX = 2;
+    private static final SecureRandom secureRandom = new SecureRandom();
+    private static final BaseEncoding hexEncoder = BaseEncoding.base16().lowerCase();
 
     private PasswordHash() {}
 
-    static String createHash(String password) throws NoSuchAlgorithmException,
-            InvalidKeySpecException {
-        return createHash(password.toCharArray());
-    }
-
-    // return salted PBKDF2 hash of the password
-    private static String createHash(char[] password) throws NoSuchAlgorithmException,
-            InvalidKeySpecException {
-        // generate a random salt
-        SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[SALT_BYTE_SIZE];
-        random.nextBytes(salt);
-
-        // hash the password
-        byte[] hash = pbkdf2(password, salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE);
-        // format iterations:salt:hash
-        return PBKDF2_ITERATIONS + ":" + toHex(salt) + ":" + toHex(hash);
+    static String createHash(String password) throws GeneralSecurityException {
+        // 128-bit salt
+        byte[] salt = new byte[16];
+        secureRandom.nextBytes(salt);
+        return hash(password, salt, ITERATION_COUNT);
     }
 
     static boolean validatePassword(String password, String correctHash)
-            throws NoSuchAlgorithmException, InvalidKeySpecException {
-        return validatePassword(password.toCharArray(), correctHash);
-    }
-
-    private static boolean validatePassword(char[] password, String correctHash)
-            throws NoSuchAlgorithmException, InvalidKeySpecException {
-        // decode the hash into its parameters
-        String[] params = correctHash.split(":");
-        int iterations = Integer.parseInt(params[ITERATION_INDEX]);
-        byte[] salt = fromHex(params[SALT_INDEX]);
-        byte[] hash = fromHex(params[PBKDF2_INDEX]);
-        // compute the hash of the provided password, using the same salt, iteration count,
-        // and hash length
-        byte[] testHash = pbkdf2(password, salt, iterations, hash.length);
-        // compare the hashes in constant time. The password is correct if both hashes match.
-        return slowEquals(hash, testHash);
-    }
-
-    // Compares two byte arrays in length-constant time. This comparison method is used so that
-    // password hashes cannot be extracted from an on-line system using a timing attack and then
-    // attacked off-line.
-    private static boolean slowEquals(byte[] a, byte[] b) {
-        int diff = a.length ^ b.length;
-        for (int i = 0; i < a.length && i < b.length; i++) {
-            diff |= a[i] ^ b[i];
+            throws GeneralSecurityException {
+        String[] correctHashParts = correctHash.split(":");
+        if (correctHashParts.length != 3) {
+            throw new GeneralSecurityException("Invalid password hash: " + correctHash);
         }
-        return diff == 0;
-    }
-
-    // computes the PBKDF2 hash of a password
-    private static byte[] pbkdf2(char[] password, byte[] salt, int iterations, int bytes)
-            throws NoSuchAlgorithmException, InvalidKeySpecException {
-        PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, bytes * 8);
-        SecretKeyFactory skf = SecretKeyFactory.getInstance(PBKDF2_ALGORITHM);
-        return skf.generateSecret(spec).getEncoded();
-    }
-
-    private static byte[] fromHex(String hex) {
-        byte[] binary = new byte[hex.length() / 2];
-        for (int i = 0; i < binary.length; i++) {
-            binary[i] = (byte) Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
+        byte[] salt;
+        try {
+            salt = hexEncoder.decode(correctHashParts[1]);
+        } catch (IllegalArgumentException e) {
+            // salt is not a valid hex encoded string
+            throw new GeneralSecurityException(e);
         }
-        return binary;
+        int iterationCount;
+        try {
+            iterationCount = Integer.parseInt(correctHashParts[2]);
+        } catch (NumberFormatException e) {
+            throw new GeneralSecurityException(e);
+        }
+        String hash = hash(password, salt, iterationCount);
+        return hash.equals(correctHash);
     }
 
-    private static String toHex(byte[] array) {
-        BigInteger bi = new BigInteger(1, array);
-        String hex = bi.toString(16);
-        int paddingLength = (array.length * 2) - hex.length();
-        if (paddingLength > 0) {
-            return String.format("%0" + paddingLength + "d", 0) + hex;
-        } else {
-            return hex;
-        }
+    private static String hash(String password, byte[] salt, int iterationCount)
+            throws GeneralSecurityException {
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, iterationCount, 128);
+        SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] hash = f.generateSecret(spec).getEncoded();
+        return hexEncoder.encode(hash) + ':' + hexEncoder.encode(salt) + ':' + ITERATION_COUNT;
     }
 }
