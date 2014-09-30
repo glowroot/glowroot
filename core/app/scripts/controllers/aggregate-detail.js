c9a09a8b608d8668e97eeb8f30f6783c2c263435
Trask Stalnaker <trask.stalnaker@gmail.com>
2014-09-29 20:37:57 -0700

    Fix sporadic modal issue

--- core/app/scripts/controllers/aggregate-detail.js
+++ core/app/scripts/controllers/aggregate-detail.js
@@ -20,19 +20,29 @@ glowroot.controller('AggregateDetailCtrl', [
   '$scope',
   '$http',
   '$modalInstance',
+  '$timeout',
   'queryStrings',
   'httpErrors',
   'aggregateQuery',
-  function ($scope, $http, $modalInstance, queryStrings, httpErrors, aggregateQuery) {
+  function ($scope, $http, $modalInstance, $timeout, queryStrings, httpErrors, aggregateQuery) {
 
     var queryString = queryStrings.encodeObject(aggregateQuery);
     $http.get('backend/aggregate/header?' + queryStrings.encodeObject(aggregateQuery))
         .success(function (data) {
           data.showExport = true;
-          HandlebarsRendering.renderAggregate(data, $('#modalContent'), queryString);
-          $scope.loaded = true;
+          // in dev mode, if the modal template 'views/aggregate-detail.html' has not already been loaded and needs to
+          // be fetch from the server, this code can be reached before the dom has been rendered, and so this code
+          // will wait (checking every 100 milliseconds) for the dom to be rendered
+          function renderAggregate() {
+            if ($('#modalContent').length === 0) {
+              $timeout(renderAggregate, 100);
+              return;
+            }
+            HandlebarsRendering.renderAggregate(data, $('#modalContent'), queryString);
+            $scope.loaded = true;
+          }
+          renderAggregate();
         })
-      // TODO need to implement httpErrors here in modal ui?
         .error(httpErrors.handler($scope));
 
     $scope.close = function () {
