37d64ce4ef6bb7429ec4b54948274cfe4ec3447d
Trask Stalnaker <trask.stalnaker@gmail.com>
2015-02-05 21:39:30 -0800

    Compact database after delete all

--- core/src/main/java/org/glowroot/local/ui/AdminJsonService.java
+++ core/src/main/java/org/glowroot/local/ui/AdminJsonService.java
@@ -88,6 +88,7 @@ class AdminJsonService {
         traceDao.deleteAll();
         gaugePointDao.deleteAll();
         aggregateDao.deleteAll();
+        dataSource.compact();
     }
 
     @POST("/backend/admin/reweave")
