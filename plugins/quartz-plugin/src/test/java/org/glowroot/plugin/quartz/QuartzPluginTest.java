b53477ab02dd4baa3725a066dba0b96b7841c84f
Trask Stalnaker <trask.stalnaker@gmail.com>
2015-03-30 18:29:01 -0700

    Add Quartz plugin

---
+++ plugins/quartz-plugin/src/test/java/org/glowroot/plugin/quartz/QuartzPluginTest.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.glowroot.plugin.quartz;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+
+import org.glowroot.Containers;
+import org.glowroot.container.AppUnderTest;
+import org.glowroot.container.Container;
+import org.glowroot.container.trace.Trace;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class QuartzPluginTest {
+
+    private static Container container;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        container = Containers.getSharedContainer();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        container.close();
+    }
+
+    @After
+    public void afterEachTest() throws Exception {
+        container.checkAndReset();
+    }
+
+    @Test
+    public void shouldCaptureJobExecution() throws Exception {
+        container.executeAppUnderTest(ExecuteJob.class);
+        Trace trace = container.getTraceService().getLastTrace();
+        assertThat(trace.getTransactionType()).isEqualTo("Background");
+        assertThat(trace.getTransactionName()).isEqualTo("Quartz job: ajob");
+        assertThat(trace.getHeadline()).isEqualTo("quartz job: ajob");
+    }
+
+    public static class ExecuteJob implements AppUnderTest {
+        @Override
+        public void executeApp() throws Exception {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = JobBuilder.newJob(TestJob.class).withIdentity("ajob", "agroup").build();
+            Trigger trigger = TriggerBuilder.newTrigger().withIdentity("atrigger", "agroup")
+                    .startNow().build();
+            scheduler.scheduleJob(job, trigger);
+            Thread.sleep(1000);
+            scheduler.shutdown();
+        }
+    }
+}
