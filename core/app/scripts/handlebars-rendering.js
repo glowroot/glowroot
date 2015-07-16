a284892c3f3e06a85a0dbb6643c9075efffc1ea1
Trask Stalnaker <trask.stalnaker@gmail.com>
2015-07-15 21:29:26 -0700

    Fix missing newline after empty message detail val
    Resolves #23

--- core/app/scripts/handlebars-rendering.js
+++ core/app/scripts/handlebars-rendering.js
@@ -210,8 +210,9 @@ HandlebarsRendering = (function () {
       } else if (typeof propVal === 'object' && propVal !== null) {
         ret += maybeBoldPropName(propName) + '<br><div class="gt-indent1">' + messageDetailHtml(propVal) + '</div>';
       } else {
-        ret += '<div style="float: left;">' + maybeBoldPropName(propName) + '&nbsp;</div>'
-            + '<div class="gt-trace-attr-value">' + propVal + '</div>';
+        // outer div with clearfix is needed when propVal is empty
+        ret += '<div class="clearfix"><div style="float: left;">' + maybeBoldPropName(propName) + '&nbsp;</div>'
+            + '<div class="gt-trace-attr-value">' + propVal + '</div></div>';
       }
     });
     return ret;
