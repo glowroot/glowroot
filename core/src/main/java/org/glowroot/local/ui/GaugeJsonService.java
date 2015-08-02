glowroot/glowroot

commit f8c06e268a19a4a823479c6b7eb49de9dd71a66b

Support string mbean attributes that parse as nums
Resolves #14

core/src/main/java/org/glowroot/local/ui/GaugeJsonService.java
@@ -25,7 +25,6 @@ import javax.management.Descriptor;
 import javax.management.MBeanAttributeInfo;
 import javax.management.MBeanInfo;
 import javax.management.MBeanServer;
-import javax.management.ObjectInstance;
 import javax.management.ObjectName;
 import javax.management.QueryExp;
 import javax.management.openmbean.CompositeData;
@@ -36,6 +35,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Ordering;
 import com.google.common.collect.Sets;
@@ -124,13 +124,13 @@ class GaugeJsonService {
             }
         }
         boolean pattern = request.mbeanObjectName().contains("*");
-        List<MBeanInfo> mbeanInfos = getMBeanInfos(request.mbeanObjectName());
-        if (mbeanInfos.isEmpty() && pattern) {
+        Set<ObjectName> objectNames = getObjectNames(request.mbeanObjectName());
+        if (objectNames.isEmpty() && pattern) {
             builder.mbeanUnmatched(true);
-        } else if (mbeanInfos.isEmpty()) {
+        } else if (objectNames.isEmpty()) {
             builder.mbeanUnavailable(true);
         } else {
-            builder.addAllMbeanAttributes(getAttributeNames(mbeanInfos));
+            builder.addAllMbeanAttributes(getAttributeNames(objectNames));
         }
         return mapper.writeValueAsString(builder.build());
     }
@@ -172,78 +172,72 @@ class GaugeJsonService {
         configService.deleteGaugeConfig(version);
     }
 
-    private GaugeResponse buildResponse(GaugeConfig gaugeConfig) throws InterruptedException {
+    private GaugeResponse buildResponse(GaugeConfig gaugeConfig) throws Exception {
         boolean pattern = gaugeConfig.mbeanObjectName().contains("*");
-        List<MBeanInfo> mbeanInfos = getMBeanInfos(gaugeConfig.mbeanObjectName());
+        Set<ObjectName> objectNames = getObjectNames(gaugeConfig.mbeanObjectName());
         GaugeResponse.Builder builder =
                 GaugeResponse.builder().config(GaugeConfigDtoBase.fromConfig(gaugeConfig));
-        if (mbeanInfos.isEmpty() && pattern) {
+        if (objectNames.isEmpty() && pattern) {
             builder.mbeanUnmatched(true);
-        } else if (mbeanInfos.isEmpty()) {
+        } else if (objectNames.isEmpty()) {
             builder.mbeanUnavailable(true);
         } else {
-            builder.addAllMbeanAvailableAttributeNames(getAttributeNames(mbeanInfos));
+            builder.addAllMbeanAvailableAttributeNames(getAttributeNames(objectNames));
         }
         return builder.build();
     }
 
-    private List<MBeanInfo> getMBeanInfos(String objectName) throws InterruptedException {
-        if (!objectName.contains("*")) {
-            try {
-                return ImmutableList.of(lazyPlatformMBeanServer.getMBeanInfo(
-                        ObjectName.getInstance(objectName)));
-            } catch (Exception e) {
-                // log exception at debug level
-                logger.debug(e.getMessage(), e);
-                return ImmutableList.of();
-            }
+    private Set<ObjectName> getObjectNames(String objectName) throws Exception {
+        if (objectName.contains("*")) {
+            return lazyPlatformMBeanServer.queryNames(null, new PatternObjectNameQueryExp(
+                    objectName));
+        } else {
+            return ImmutableSet.of(ObjectName.getInstance(objectName));
         }
-        Set<ObjectInstance> objectInstances = lazyPlatformMBeanServer.queryMBeans(null,
-                new PatternObjectNameQueryExp(objectName));
-        List<MBeanInfo> mbeanInfos = Lists.newArrayList();
-        for (ObjectInstance objectInstance : objectInstances) {
-            try {
-                mbeanInfos.add(lazyPlatformMBeanServer.getMBeanInfo(
-                        objectInstance.getObjectName()));
-            } catch (Exception e) {
-                // log exception at debug level
-                logger.debug(e.getMessage(), e);
-            }
-        }
-        return mbeanInfos;
     }
 
-    private static Set<String> getAttributeNames(List<MBeanInfo> mbeanInfos) {
+    private Set<String> getAttributeNames(Set<ObjectName> objectNames) throws Exception {
         Set<String> attributeNames = Sets.newHashSet();
-        for (MBeanInfo mbeanInfo : mbeanInfos) {
+        for (ObjectName objectName : objectNames) {
+            MBeanInfo mbeanInfo = lazyPlatformMBeanServer.getMBeanInfo(objectName);
             for (MBeanAttributeInfo attribute : mbeanInfo.getAttributes()) {
                 if (attribute.isReadable()) {
-                    addNumericAttributes(attribute, attributeNames);
+                    Object value =
+                            lazyPlatformMBeanServer.getAttribute(objectName, attribute.getName());
+                    addNumericAttributes(attribute, value, attributeNames);
                 }
             }
         }
         return attributeNames;
     }
 
-    private static void addNumericAttributes(MBeanAttributeInfo attribute,
+    private static void addNumericAttributes(MBeanAttributeInfo attribute, Object value,
             Set<String> attributeNames) {
         String attributeType = attribute.getType();
         if (attributeType.equals("long") || attributeType.equals("int")
                 || attributeType.equals("double") || attributeType.equals("float")) {
             attributeNames.add(attribute.getName());
+        } else if (attributeType.equals("java.lang.String") && value instanceof String) {
+            try {
+                Double.parseDouble((String) value);
+                attributeNames.add(attribute.getName());
+            } catch (NumberFormatException e) {
+                // log exception at debug level
+                logger.debug(e.getMessage(), e);
+            }
         } else if (attributeType.equals(CompositeData.class.getName())) {
             Descriptor descriptor = attribute.getDescriptor();
             Object descriptorFieldValue = descriptor.getFieldValue("openType");
             if (descriptorFieldValue instanceof CompositeType) {
                 CompositeType compositeType = (CompositeType) descriptorFieldValue;
-                attributeNames.addAll(getCompositeTypeAttributeNames(attribute,
+                attributeNames.addAll(getCompositeTypeAttributeNames(attribute, value,
                         compositeType));
             }
         }
     }
 
     private static List<String> getCompositeTypeAttributeNames(MBeanAttributeInfo attribute,
-            CompositeType compositeType) {
+            Object compositeData, CompositeType compositeType) {
         List<String> attributeNames = Lists.newArrayList();
         for (String itemName : compositeType.keySet()) {
             OpenType<?> itemType = compositeType.getType(itemName);
@@ -260,6 +254,17 @@ class GaugeJsonService {
             }
             if (Number.class.isAssignableFrom(clazz)) {
                 attributeNames.add(attribute.getName() + '/' + itemName);
+            } else if (clazz == String.class && compositeData instanceof CompositeData) {
+                Object val = ((CompositeData) compositeData).get(itemName);
+                if (val instanceof String) {
+                    try {
+                        Double.parseDouble((String) val);
+                        attributeNames.add(attribute.getName() + '/' + itemName);
+                    } catch (NumberFormatException e) {
+                        // log exception at debug level
+                        logger.debug(e.getMessage(), e);
+                    }
+                }
             }
         }
         return attributeNames;

core/src/main/java/org/glowroot/collector/GaugeCollector.java
@@ -29,7 +29,6 @@ import javax.management.AttributeNotFoundException;
 import javax.management.InstanceNotFoundException;
 import javax.management.MBeanServer;
 import javax.management.MalformedObjectNameException;
-import javax.management.ObjectInstance;
 import javax.management.ObjectName;
 import javax.management.openmbean.CompositeData;
 
@@ -158,15 +157,14 @@ class GaugeCollector extends ScheduledRunnable {
             }
             return collectGaugePoints(objectName, gaugeConfig.mbeanAttributes(), mbeanObjectName);
         }
-        Set<ObjectInstance> objectInstances = lazyPlatformMBeanServer.queryMBeans(null,
+        Set<ObjectName> objectNames = lazyPlatformMBeanServer.queryNames(null,
                 new PatternObjectNameQueryExp(mbeanObjectName));
-        if (objectInstances.isEmpty()) {
+        if (objectNames.isEmpty()) {
             logFirstTimeMBeanNotMatchedOrFound(mbeanObjectName);
             return ImmutableList.of();
         }
         List<GaugePoint> gaugePoints = Lists.newArrayList();
-        for (ObjectInstance objectInstance : objectInstances) {
-            ObjectName objectName = objectInstance.getObjectName();
+        for (ObjectName objectName : objectNames) {
             gaugePoints.addAll(collectGaugePoints(objectName, gaugeConfig.mbeanAttributes(),
                     objectName.getDomain() + ":" + objectName.getKeyPropertyListString()));
         }
@@ -206,17 +204,28 @@ class GaugeCollector extends ScheduledRunnable {
                 logFirstTimeMBeanAttributeError(mbeanObjectName, mbeanAttributeName, e.toString());
                 continue;
             }
+            Double value = null;
             if (attributeValue instanceof Number) {
-                double value = ((Number) attributeValue).doubleValue();
+                value = ((Number) attributeValue).doubleValue();
+            } else if (attributeValue instanceof String) {
+                try {
+                    value = Double.parseDouble((String) attributeValue);
+                } catch (NumberFormatException e) {
+                    logFirstTimeMBeanAttributeError(mbeanObjectName, mbeanAttributeName,
+                            "MBean attribute value is not a valid number: \"" + attributeValue
+                                    + "\"");
+                }
+            } else {
+                logFirstTimeMBeanAttributeError(mbeanObjectName, mbeanAttributeName,
+                        "MBean attribute value is not a number or string");
+            }
+            if (value != null) {
                 gaugePoints.add(GaugePoint.builder()
                         .gaugeName(mbeanObjectName + ',' + mbeanAttributeName)
                         .everIncreasing(mbeanAttribute.everIncreasing())
                         .captureTime(captureTime)
                         .value(value)
                         .build());
-            } else {
-                logFirstTimeMBeanAttributeError(mbeanObjectName, mbeanAttributeName,
-                        "MBean attribute value is not a number");
             }
         }
         return gaugePoints;

core/src/main/java/org/glowroot/jvm/LazyPlatformMBeanServer.java
@@ -51,9 +51,9 @@ public class LazyPlatformMBeanServer {
         this.jbossModules = jbossModules;
     }
 
-    void getObjectInstance(ObjectName name) throws Exception {
+    public ObjectInstance getObjectInstance(ObjectName name) throws Exception {
         ensureInit();
-        mbeanServer.getObjectInstance(name);
+        return mbeanServer.getObjectInstance(name);
     }
 
     void invoke(ObjectName name, String operationName, Object[] params, String[] signature)
@@ -66,12 +66,6 @@ public class LazyPlatformMBeanServer {
             throws InterruptedException {
         ensureInit();
         return mbeanServer.queryNames(name, query);
-    }
-
-    public Set<ObjectInstance> queryMBeans(@Nullable ObjectName name, @Nullable QueryExp query)
-            throws InterruptedException {
-        ensureInit();
-        return mbeanServer.queryMBeans(name, query);
     }
 
     public MBeanInfo getMBeanInfo(ObjectName name) throws Exception {

core/src/test/java/org/glowroot/collector/GaugeCollectorTest.java
@@ -217,8 +217,31 @@ public class GaugeCollectorTest {
         gaugeCollector.collectGaugePoints(gaugeConfig);
         // then
         verify(logger).warn("error accessing mbean attribute {} {}: {}", "xyz:aaa=bbb", "ccc",
-                "MBean attribute value is not a number");
+                "MBean attribute value is not a valid number: \"not a number\"");
         verify(logger).warn("error accessing mbean attribute {} {}: {}", "xyz:aaa=bbb", "ddd",
-                "MBean attribute value is not a number");
+                "MBean attribute value is not a valid number: \"not a number\"");
+    }
+
+    @Test
+    public void shouldHandleMBeanAttributeNotANumberOrString() throws Exception {
+        // given
+        GaugeConfig gaugeConfig = GaugeConfig.builder()
+                .mbeanObjectName("xyz:aaa=bbb")
+                .addMbeanAttributes(MBeanAttribute.of("ccc", false))
+                .addMbeanAttributes(MBeanAttribute.of("ddd", false))
+                .build();
+        when(lazyPlatformMBeanServer.getAttribute(any(ObjectName.class), anyString()))
+                .thenReturn(new Object());
+        // when
+        gaugeCollector.collectGaugePoints(gaugeConfig);
+        gaugeCollector.collectGaugePoints(gaugeConfig);
+        gaugeCollector.collectGaugePoints(gaugeConfig);
+        gaugeCollector.collectGaugePoints(gaugeConfig);
+        gaugeCollector.collectGaugePoints(gaugeConfig);
+        // then
+        verify(logger).warn("error accessing mbean attribute {} {}: {}", "xyz:aaa=bbb", "ccc",
+                "MBean attribute value is not a number or string");
+        verify(logger).warn("error accessing mbean attribute {} {}: {}", "xyz:aaa=bbb", "ddd",
+                "MBean attribute value is not a number or string");
     }
 }

commit 05b3cafd141fd08828ec0bd76668ba83a1a75352

Handle exception in check for num-ish mbean vals
Regression caused by f8c06e2

Resolves #25

core/src/main/java/org/glowroot/local/ui/GaugeJsonService.java
@@ -199,15 +199,31 @@ class GaugeJsonService {
         }
     }
 
-    private Set<String> getAttributeNames(Set<ObjectName> objectNames) throws Exception {
+    private Set<String> getAttributeNames(Set<ObjectName> objectNames) {
         Set<String> attributeNames = Sets.newHashSet();
         for (ObjectName objectName : objectNames) {
-            MBeanInfo mbeanInfo = lazyPlatformMBeanServer.getMBeanInfo(objectName);
-            for (MBeanAttributeInfo attribute : mbeanInfo.getAttributes()) {
-                if (attribute.isReadable()) {
-                    Object value =
-                            lazyPlatformMBeanServer.getAttribute(objectName, attribute.getName());
+            try {
+                MBeanInfo mbeanInfo = lazyPlatformMBeanServer.getMBeanInfo(objectName);
+                attributeNames.addAll(getAttributeNames(mbeanInfo, objectName));
+            } catch (Exception e) {
+                // log exception at debug level
+                logger.debug(e.getMessage(), e);
+            }
+        }
+        return attributeNames;
+    }
+
+    private Set<String> getAttributeNames(MBeanInfo mbeanInfo, ObjectName objectName) {
+        Set<String> attributeNames = Sets.newHashSet();
+        for (MBeanAttributeInfo attribute : mbeanInfo.getAttributes()) {
+            if (attribute.isReadable()) {
+                try {
+                    Object value = lazyPlatformMBeanServer.getAttribute(objectName,
+                            attribute.getName());
                     addNumericAttributes(attribute, value, attributeNames);
+                } catch (Exception e) {
+                    // log exception at debug level
+                    logger.debug(e.getMessage(), e);
                 }
             }
         }