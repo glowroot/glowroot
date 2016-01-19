4f741400d7c2bd471d93ba3c1ddc20c95c4cd885
Albert-Jan de Vries <code@albert-jandevries.nl>
2016-01-19 14:36:10 +0100

    Add Spring plugin

---
+++ agent-parent/plugins/spring-plugin/src/test/java/org/glowroot/agent/plugin/spring/ControllerIT.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.glowroot.agent.plugin.spring;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.glowroot.agent.it.harness.AppUnderTest;
+import org.glowroot.agent.it.harness.Container;
+import org.glowroot.agent.it.harness.Containers;
+import org.glowroot.agent.it.harness.TransactionMarker;
+import org.glowroot.wire.api.model.TraceOuterClass.Trace;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ControllerIT {
+
+    private static Container container;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        container = Containers.create();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        container.close();
+    }
+
+    @After
+    public void afterEachTest() throws Exception {
+        container.checkAndReset();
+    }
+
+    @Test
+    public void shouldCaptureTransactionName() throws Exception {
+        // given
+        // when
+        Trace trace = container.execute(ExecuteController.class);
+        // then
+        assertThat(trace.getHeader().getTransactionName()).isEqualTo("TestController/testMethod");
+        List<Trace.Entry> entries = trace.getEntryList();
+        assertThat(entries).hasSize(1);
+        Trace.Entry entry = entries.get(0);
+        assertThat(entry.getMessage())
+                .isEqualTo("spring controller: TestController.testMethod()");
+    }
+
+    public static class ExecuteController implements AppUnderTest, TransactionMarker {
+        @Override
+        public void executeApp() throws Exception {
+            transactionMarker();
+        }
+        @Override
+        public void transactionMarker() throws Exception {
+            new TestController().testMethod();
+        }
+    }
+
+    @Controller
+    public static class TestController {
+        @RequestMapping("/abc")
+        public void testMethod() {}
+    }
+}
