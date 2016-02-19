8d3e96f8320423e876344aba2255ab8631e9270d
Albert-Jan de Vries <code@albert-jandevries.nl>
2016-02-19 15:15:38 +0100

    Add Jedis plugin
    Closes #68

---
+++ agent-parent/plugins/jedis-plugin/src/test/java/org/glowroot/agent/plugin/jedis/ConnectionIT.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.glowroot.agent.plugin.jedis;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+import org.glowroot.agent.it.harness.AppUnderTest;
+import org.glowroot.agent.it.harness.Container;
+import org.glowroot.agent.it.harness.Containers;
+import org.glowroot.agent.it.harness.TransactionMarker;
+import org.glowroot.wire.api.model.TraceOuterClass.Trace;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConnectionIT {
+
+    private static Container container;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        container = Containers.create();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        container.close();
+    }
+
+    @After
+    public void afterEachTest() throws Exception {
+        container.checkAndReset();
+    }
+
+    @Test
+    public void shouldTraceSet() throws Exception {
+        // given
+        // when
+        Trace trace = container.execute(JedisSet.class);
+        // then
+        List<Trace.Entry> entries = trace.getEntryList();
+        assertThat(entries).hasSize(1);
+        Trace.Entry entry = entries.get(0);
+        assertThat(entry.getMessage()).matches("jedis localhost:\\d+ SET");
+    }
+
+    @Test
+    public void shouldTraceGet() throws Exception {
+        // given
+        // when
+        Trace trace = container.execute(JedisGet.class);
+        // then
+        List<Trace.Entry> entries = trace.getEntryList();
+        assertThat(entries).hasSize(1);
+        Trace.Entry entry = entries.get(0);
+        assertThat(entry.getMessage()).matches("jedis localhost:\\d+ GET");
+    }
+
+    @Test
+    public void shouldTracePing() throws Exception {
+        // given
+        // when
+        Trace trace = container.execute(JedisPing.class);
+        // then
+        List<Trace.Entry> entries = trace.getEntryList();
+        assertThat(entries).hasSize(1);
+        Trace.Entry entry = entries.get(0);
+        assertThat(entry.getMessage()).matches("jedis localhost:\\d+ PING");
+    }
+
+    private static abstract class JedisBase implements AppUnderTest, TransactionMarker {
+
+        private RedisMockServer redisMockServer;
+
+        private Jedis jedis;
+
+        @Override
+        public void executeApp() throws Exception {
+            redisMockServer = new RedisMockServer();
+            jedis = new Jedis("localhost", redisMockServer.getPort());
+            transactionMarker();
+            redisMockServer.close();
+        }
+
+        protected Jedis getJedis() {
+            return jedis;
+        }
+    }
+
+    public static class JedisSet extends JedisBase implements TransactionMarker {
+        @Override
+        public void transactionMarker() {
+            getJedis().set("key", "value");
+        }
+    }
+
+    public static class JedisGet extends JedisBase implements TransactionMarker {
+        @Override
+        public void transactionMarker() {
+            getJedis().get("key");
+        }
+    }
+
+    public static class JedisPing extends JedisBase implements TransactionMarker {
+        @Override
+        public void transactionMarker() {
+            getJedis().ping();
+        }
+    }
+}
---
+++ agent-parent/plugins/jedis-plugin/src/test/java/org/glowroot/agent/plugin/jedis/RedisMockServer.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.glowroot.agent.plugin.jedis;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class RedisMockServer implements Runnable {
+
+    private static final Logger logger = LoggerFactory.getLogger(RedisMockServer.class);
+
+    private final ExecutorService executor = Executors.newCachedThreadPool();
+
+    private final ServerSocket server;
+
+    private int port;
+    private boolean stop;
+
+    RedisMockServer() throws IOException {
+        server = new ServerSocket(0);
+        port = server.getLocalPort();
+        executor.execute(this);
+    }
+
+    @Override
+    public void run() {
+        try {
+            runInternal();
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    int getPort() {
+        return port;
+    }
+
+    void close() {
+        stop = true;
+        executor.shutdownNow();
+    }
+
+    private void runInternal() throws IOException {
+        while (!stop) {
+            Socket socket = server.accept();
+            executor.execute(new CallResponseProxy(socket));
+        }
+    }
+
+    private static class CallResponseProxy implements Runnable {
+
+        private Socket socket;
+
+        CallResponseProxy(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                runInternal();
+            } catch (IOException e) {
+                logger.error(e.getMessage(), e);
+            }
+        }
+
+        private void runInternal() throws IOException, UnsupportedEncodingException {
+            byte[] request = new byte[1024];
+            InputStream socketIn = socket.getInputStream();
+            OutputStream socketOut = socket.getOutputStream();
+            int nBytes;
+            while ((nBytes = socketIn.read(request)) != -1) {
+                String command = new String(request, 0, nBytes, "UTF-8").replaceAll("\r\n", " ");
+                String response = getResponse(command);
+                socketOut.write(response.getBytes());
+            }
+        }
+
+        private static String getResponse(String command) {
+            if (command.equals("*1 $4 PING ")) {
+                return "+PONG\r\n";
+            } else if (command.equals("*2 $3 GET $3 key ")) {
+                return "$5\r\nvalue\r\n";
+            } else if (command.equals("*3 $3 SET $3 key $5 value ")) {
+                return "+OK\r\n";
+            } else {
+                return "+OK\r\n";
+            }
+        }
+    }
+}
