c822802f8c4a988b75d6916f0e84622f0bd781fd
Albert-Jan de Vries <code@albert-jandevries.nl>
2016-03-17 19:30:06 +0100

    Add http client support for Spring RestTemplate
    Closes #88

---
+++ agent-parent/plugins/http-client-plugin/src/main/java/org/glowroot/agent/plugin/httpclient/RestTemplateAspect.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.glowroot.agent.plugin.httpclient;
+
+import java.net.URI;
+
+import javax.annotation.Nullable;
+
+import org.glowroot.agent.plugin.api.Agent;
+import org.glowroot.agent.plugin.api.MessageSupplier;
+import org.glowroot.agent.plugin.api.ThreadContext;
+import org.glowroot.agent.plugin.api.TimerName;
+import org.glowroot.agent.plugin.api.TraceEntry;
+import org.glowroot.agent.plugin.api.weaving.BindParameter;
+import org.glowroot.agent.plugin.api.weaving.BindThrowable;
+import org.glowroot.agent.plugin.api.weaving.BindTraveler;
+import org.glowroot.agent.plugin.api.weaving.OnBefore;
+import org.glowroot.agent.plugin.api.weaving.OnReturn;
+import org.glowroot.agent.plugin.api.weaving.OnThrow;
+import org.glowroot.agent.plugin.api.weaving.Pointcut;
+import org.glowroot.agent.plugin.api.weaving.Shim;
+
+public class RestTemplateAspect {
+
+    @Shim("org.springframework.http.HttpMethod")
+    public interface HttpMethod {
+        String name();
+    }
+
+    @Pointcut(className = "org.springframework.web.client.RestTemplate", methodName = "doExecute",
+            methodParameterTypes = {"java.net.URI", "org.springframework.http.HttpMethod", ".."},
+            nestingGroup = "http-client", timerName = "http client request")
+    public static class ExecuteAdvice {
+        private static final TimerName timerName = Agent.getTimerName(ExecuteAdvice.class);
+        @OnBefore
+        public static @Nullable TraceEntry onBefore(ThreadContext context, @BindParameter URI uri,
+                @BindParameter HttpMethod method) {
+            String url = uri.toString();
+            return context.startServiceCallEntry("HTTP", method + " " + Uris.stripQueryString(url),
+                    MessageSupplier.from("http client request: {} {}", method.name(), url),
+                    timerName);
+        }
+        @OnReturn
+        public static void onReturn(@BindTraveler @Nullable TraceEntry traceEntry) {
+            if (traceEntry != null) {
+                traceEntry.end();
+            }
+        }
+        @OnThrow
+        public static void onThrow(@BindThrowable Throwable throwable,
+                @BindTraveler @Nullable TraceEntry traceEntry) {
+            if (traceEntry != null) {
+                traceEntry.endWithError(throwable);
+            }
+        }
+    }
+}
