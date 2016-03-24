3d5823e0bc15d99cb3a583cd7029da7aec394d98
Trask Stalnaker <trask.stalnaker@gmail.com>
2016-03-23 21:25:27 -0700

    Use weak refs in TransactionCollection
    In case async transaction never ends itself, this will keep
    TransactionCollection from retaining the transaction forever.

--- agent-parent/agent/src/main/java/org/glowroot/agent/impl/TransactionCollection.java
+++ agent-parent/agent/src/main/java/org/glowroot/agent/impl/TransactionCollection.java
@@ -15,6 +15,9 @@
  */
 package org.glowroot.agent.impl;
 
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -26,74 +29,137 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 public class TransactionCollection implements Iterable<Transaction> {
 
-    private final TransactionEntry head = new TransactionEntry(null);
+    private final ReferenceQueue<Transaction> queue = new ReferenceQueue<Transaction>();
+
+    private final TransactionEntry headEntry = new TransactionEntry(null, queue);
 
     // tail is non-volatile since only accessed under lock
-    private TransactionEntry tail = head;
+    private TransactionEntry tailEntry = headEntry;
 
     // all structural changes are made under lock for simplicity
     // TODO implement lock free structure
     private final Object lock = new Object();
 
     public TransactionEntry add(Transaction transaction) {
-        TransactionEntry newTailEntry = new TransactionEntry(transaction);
+        TransactionEntry newTailEntry = new TransactionEntry(transaction, queue);
         synchronized (lock) {
-            tail.next = newTailEntry;
-            newTailEntry.prev = tail;
-            tail = newTailEntry;
+            expungeStaleEntries();
+            tailEntry.nextEntry = newTailEntry;
+            newTailEntry.prevEntry = tailEntry;
+            tailEntry = newTailEntry;
         }
         return newTailEntry;
     }
 
     @Override
     public Iterator<Transaction> iterator() {
-        return new Iterator<Transaction>() {
-            private @Nullable TransactionEntry next = head.next;
-            @Override
-            public boolean hasNext() {
-                return next != null;
+        synchronized (lock) {
+            expungeStaleEntries();
+        }
+        return new TransactionIterator();
+    }
+
+    // requires lock
+    private void expungeStaleEntries() {
+        Reference<? extends Transaction> ref = queue.poll();
+        if (ref == null) {
+            return;
+        }
+        // drain the queue, since going to loop over and clean up everything anyways
+        while (queue.poll() != null) {
+        }
+        TransactionEntry currEntry = headEntry.nextEntry;
+        while (currEntry != null) {
+            if (currEntry.getTransaction() == null) {
+                currEntry.remove();
             }
-            @Override
-            public Transaction next() {
-                TransactionEntry curr = next;
-                if (curr == null) {
-                    throw new NoSuchElementException();
-                }
-                next = curr.next;
-                return checkNotNull(curr.transaction);
+            currEntry = currEntry.nextEntry;
+        }
+    }
+
+    private class TransactionIterator implements Iterator<Transaction> {
+
+        private @Nullable TransactionEntry nextEntry;
+        private @Nullable Transaction nextTransaction;
+
+        @SuppressWarnings("method.invocation.invalid")
+        private TransactionIterator() {
+            nextEntry = headEntry;
+            advance();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return nextTransaction != null;
+        }
+
+        @Override
+        public Transaction next() {
+            Transaction currTransaction = nextTransaction;
+            if (currTransaction == null) {
+                throw new NoSuchElementException();
             }
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException();
+            advance();
+            return currTransaction;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        private void advance() {
+            advanceOne();
+            while (nextTransaction == null && nextEntry != null) {
+                advanceOne();
             }
-        };
+        }
+
+        private void advanceOne() {
+            checkNotNull(nextEntry);
+            nextEntry = nextEntry.nextEntry;
+            nextTransaction = nextEntry == null ? null : nextEntry.getTransaction();
+        }
     }
 
     public class TransactionEntry {
 
-        private final @Nullable Transaction transaction; // only null for head
+        private final @Nullable WeakReference<Transaction> transactionRef; // only null for head
 
         // prev is non-volatile since only accessed under lock
-        private @Nullable TransactionEntry prev;
+        private @Nullable TransactionEntry prevEntry;
 
         // next is volatile since accessed by iterator outside of lock
-        private volatile @Nullable TransactionEntry next;
+        private volatile @Nullable TransactionEntry nextEntry;
 
-        private TransactionEntry(@Nullable Transaction transaction) {
-            this.transaction = transaction;
+        private TransactionEntry(@Nullable Transaction transaction,
+                ReferenceQueue<Transaction> queue) {
+            if (transaction == null) {
+                transactionRef = null;
+            } else {
+                transactionRef = new WeakReference<Transaction>(transaction, queue);
+            }
         }
 
         public void remove() {
-            checkNotNull(prev);
             synchronized (lock) {
-                prev.next = next;
-                if (next != null) {
-                    next.prev = prev;
+                expungeStaleEntries();
+                TransactionEntry localPrevEntry = checkNotNull(prevEntry);
+                localPrevEntry.nextEntry = nextEntry;
+                if (nextEntry != null) {
+                    nextEntry.prevEntry = localPrevEntry;
                 }
-                if (this == tail) {
-                    tail = prev;
+                if (this == tailEntry) {
+                    tailEntry = localPrevEntry;
                 }
             }
+        }
+
+        private @Nullable Transaction getTransaction() {
+            if (transactionRef == null) {
+                return null;
+            }
+            return transactionRef.get();
         }
     }
 }
