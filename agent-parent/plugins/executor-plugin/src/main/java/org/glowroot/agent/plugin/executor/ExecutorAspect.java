85a2314454b8a8105f0b6c4cdaf40b0d3bd9b937
Trask Stalnaker <trask.stalnaker@gmail.com>
2016-04-04 23:11:53 -0700

    Capture Akka scheduler

--- agent-parent/plugins/executor-plugin/src/main/java/org/glowroot/agent/plugin/executor/ExecutorAspect.java
+++ agent-parent/plugins/executor-plugin/src/main/java/org/glowroot/agent/plugin/executor/ExecutorAspect.java
@@ -136,6 +136,28 @@ public class ExecutorAspect {
         }
     }
 
+    // no nesting group in order to capture sometimes wrapped runnable passed to delegate executor
+    @Pointcut(className = "akka.actor.Scheduler", methodName = "scheduleOnce",
+            methodParameterTypes = {"scala.concurrent.duration.FiniteDuration",
+                    "java.lang.Runnable", ".."})
+    public static class ScheduleOnceAdvice {
+        @IsEnabled
+        public static boolean isEnabled(@SuppressWarnings("unused") @BindParameter Object duration,
+                @BindParameter Object runnableCallable) {
+            // this class may have been loaded before class file transformer was added to jvm
+            return runnableCallable instanceof RunnableEtcMixin
+                    && !(runnableCallable instanceof SuppressedRunnableMixin);
+        }
+        @OnBefore
+        public static void onBefore(ThreadContext context,
+                @SuppressWarnings("unused") @BindParameter Object duration,
+                @BindParameter Object runnableCallable) {
+            RunnableEtcMixin runnableCallableMixin = (RunnableEtcMixin) runnableCallable;
+            AuxThreadContext auxContext = context.createAuxThreadContext();
+            runnableCallableMixin.glowroot$setAuxContext(auxContext);
+        }
+    }
+
     // TODO revisit this
     // this method uses submit() and returns Future, but none of the callers use/wait on the Future
     @Pointcut(className = "net.sf.ehcache.store.disk.DiskStorageFactory", methodName = "schedule",
