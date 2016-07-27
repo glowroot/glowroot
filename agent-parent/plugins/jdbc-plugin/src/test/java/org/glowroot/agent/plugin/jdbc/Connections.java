99b03d558372e99d36748b7a450cc11caa542a70
Trask Stalnaker <trask.stalnaker@gmail.com>
2016-07-27 15:08:50 -0700

    Minor test fix

--- agent-parent/plugins/jdbc-plugin/src/test/java/org/glowroot/agent/plugin/jdbc/Connections.java
+++ agent-parent/plugins/jdbc-plugin/src/test/java/org/glowroot/agent/plugin/jdbc/Connections.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 the original author or authors.
+ * Copyright 2015-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -158,7 +158,7 @@ public class Connections {
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         Connection connection =
                 DriverManager.getConnection("jdbc:sqlserver://localhost", "sa", "password");
-        insertRecords(connection);
+        insertRecords(connection, "varbinary(max)", "varchar(max)");
         return connection;
     }
 
