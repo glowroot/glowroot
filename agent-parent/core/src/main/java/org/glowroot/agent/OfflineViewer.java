f088d6f1077bf5dfaaf20c70504670439db3c999
Trask Stalnaker <trask.stalnaker@gmail.com>
2016-08-28 20:15:56 -0700

    Add recover tool for offline viewer
    When glowroot h2 database copied from live system, the copy may be in an
    invalid state and require h2 recover tool to be run against it.

--- agent-parent/core/src/main/java/org/glowroot/agent/OfflineViewer.java
+++ agent-parent/core/src/main/java/org/glowroot/agent/OfflineViewer.java
@@ -18,18 +18,30 @@ package org.glowroot.agent;
 import java.io.File;
 import java.net.URISyntaxException;
 import java.security.CodeSource;
+import java.sql.SQLException;
 
 import javax.annotation.Nullable;
 
 import com.google.common.annotations.VisibleForTesting;
+import org.h2.tools.Recover;
+import org.h2.tools.RunScript;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-class OfflineViewer {
+public class OfflineViewer {
+
+    private static final Logger startupLogger = LoggerFactory.getLogger("org.glowroot");
 
     private OfflineViewer() {}
 
     public static void main(String... args) throws Exception {
         CodeSource codeSource = OfflineViewer.class.getProtectionDomain().getCodeSource();
-        MainEntryPoint.runViewer(getGlowrootJarFile(codeSource));
+        File glowrootJarFile = getGlowrootJarFile(codeSource);
+        if (args.length == 1 && args[0].equals("recover")) {
+            recover(glowrootJarFile);
+            return;
+        }
+        MainEntryPoint.runViewer(glowrootJarFile);
     }
 
     @VisibleForTesting
@@ -43,5 +55,43 @@ class OfflineViewer {
             return codeSourceFile;
         }
         return null;
+    }
+
+    private static void recover(@Nullable File glowrootJarFile) throws SQLException {
+        String baseDirPath = System.getProperty("glowroot.base.dir");
+        File baseDir = BaseDir.getBaseDir(baseDirPath, glowrootJarFile);
+        File dataDir = new File(baseDir, "data");
+        File recoverFile = new File(dataDir, "data.h2.sql");
+        if (recoverFile.exists()) {
+            if (!recoverFile.delete()) {
+                startupLogger.warn("recover failed: cannot delete existing data.h2.sql");
+            }
+        }
+        Recover.main(new String[] {"-dir", dataDir.getPath(), "-db", "data"});
+        File dbFile = new File(dataDir, "data.h2.db");
+        File dbBakFile = new File(dataDir, "data.h2.db.bak");
+        if (dbBakFile.exists()) {
+            if (!dbBakFile.delete()) {
+                startupLogger.warn("recover failed, cannot delete existing file: {}",
+                        dbBakFile.getPath());
+            }
+        }
+        if (!dbFile.renameTo(dbBakFile)) {
+            startupLogger.warn("recover failed, cannot rename {} to {}", dbFile.getPath(),
+                    dbBakFile.getPath());
+            return;
+        }
+        RunScript.main(
+                new String[] {"-url", "jdbc:h2:" + dataDir.getPath() + File.separator + "data",
+                        "-script", recoverFile.getPath()});
+        startupLogger.info("recover succeeded");
+
+        // clean up
+        if (!dbBakFile.delete()) {
+            startupLogger.info("failed to clean-up, cannot delete file: {}", dbBakFile.getPath());
+        }
+        if (!recoverFile.delete()) {
+            startupLogger.info("failed to clean-up, cannot delete file: {}", recoverFile.getPath());
+        }
     }
 }
