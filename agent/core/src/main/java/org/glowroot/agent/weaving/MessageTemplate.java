82e1110933d0b745e31cca83edbaa38163bde444
Trask Stalnaker <trask.stalnaker@gmail.com>
2017-01-19 22:43:13 -0800

    Very minor cleanup

--- agent/core/src/main/java/org/glowroot/agent/weaving/MessageTemplate.java
+++ agent/core/src/main/java/org/glowroot/agent/weaving/MessageTemplate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2016 the original author or authors.
+ * Copyright 2013-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,8 +60,7 @@ public class MessageTemplate {
             if (matcher.start() > curr) {
                 allParts.add(new ConstantPart(template.substring(curr, matcher.start())));
             }
-            String group = matcher.group(1);
-            checkNotNull(group);
+            String group = checkNotNull(matcher.group(1));
             String path = group.trim();
             int index = path.indexOf('.');
             String base;
