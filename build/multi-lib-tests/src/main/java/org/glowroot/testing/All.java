d756ff8a752c86eca4419f9586d0ee3f03f5cc3a
Trask Stalnaker <trask.stalnaker@gmail.com>
2017-04-10 10:31:19 -0700

    Remove Spray plugin from multi-lib-tests

--- build/multi-lib-tests/src/main/java/org/glowroot/testing/All.java
+++ build/multi-lib-tests/src/main/java/org/glowroot/testing/All.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,7 +35,6 @@ public class All {
         Quartz.main(args);
         Redis.main(args);
         Servlet.main(args);
-        Spray.main(args);
         Spring.main(args);
         Struts.main(args);
     }
