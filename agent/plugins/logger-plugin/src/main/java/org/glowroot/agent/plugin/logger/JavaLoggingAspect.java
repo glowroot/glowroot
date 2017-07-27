c6ce2c67c8de52c350604ac30da67ac3de20ebda
Alberto <albertus82@gmail.com>
2017-07-27 11:19:51 +0200

    Logger Plugin: added java.util.logging support.

--- agent/plugins/logger-plugin/src/main/java/org/glowroot/agent/plugin/logger/JavaLoggingAspect.java
+++ agent/plugins/logger-plugin/src/main/java/org/glowroot/agent/plugin/logger/JavaLoggingAspect.java
@@ -15,6 +15,11 @@
  */
 package org.glowroot.agent.plugin.logger;
 
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import org.glowroot.agent.plugin.api.Agent;
@@ -27,51 +32,32 @@ import org.glowroot.agent.plugin.api.weaving.BindTraveler;
 import org.glowroot.agent.plugin.api.weaving.OnAfter;
 import org.glowroot.agent.plugin.api.weaving.OnBefore;
 import org.glowroot.agent.plugin.api.weaving.Pointcut;
-import org.glowroot.agent.plugin.api.weaving.Shim;
 
 public class JavaLoggingAspect {
 
-    private static final String TIMER_NAME = "logging";
-
-    // constants from org.apache.log4j.Priority
-    private static final int OFF_INT = Integer.MAX_VALUE;
-    private static final int ERROR_INT = 1000;
-    private static final int WARN_INT = 900;
-    private static final int INFO_INT = 800;
-    private static final int CONFIG_INT = 700;
-    private static final int FINE_INT = 500;
-    private static final int FINER_INT = 400;
-    private static final int FINEST_INT = 300;
-    private static final int ALL_INT = Integer.MIN_VALUE;
-    
-  @Shim("java.util.logging.LogRecord")
-  public interface LogRecord {
-
-      @Shim("java.util.logging.Level getLevel()")
-      @Nullable
-      Level glowroot$getLevel();
-
-      @Nullable
-      String getMessage();
-
-      @Nullable
-      String getLoggerName();
-
-      @Nullable
-      Throwable getThrown();
-  }
-
-    @Shim("java.util.logging.Level")
-    public interface Level {
-        int intValue();
+    private JavaLoggingAspect() {
+        throw new IllegalAccessError(); // prevent instantiation
     }
+
+    private static final String TIMER_NAME = "logging";
 
     @Pointcut(className = "java.util.logging.Logger", methodName = "log",
             methodParameterTypes = {"java.util.logging.LogRecord"},
             nestingGroup = "logging", timerName = TIMER_NAME)
-    public static class CallAppendersAdvice {
+    public static class LogAdvice {
 
-        private static final TimerName timerName = Agent.getTimerName(CallAppendersAdvice.class);
+        private LogAdvice() {
+            throw new IllegalAccessError(); // prevent instantiation
+        }
+
+        private static final TimerName timerName = Agent.getTimerName(LogAdvice.class);
+
+        private static final Formatter formatter = new Formatter() {
+            @Override
+            public String format(@Nonnull LogRecord record) {
+                return formatMessage(record);
+            }
+        };
 
         @OnBefore
         public static @Nullable LogAdviceTraveler onBefore(ThreadContext context,
@@ -79,17 +65,16 @@ public class JavaLoggingAspect {
             if (loggingEvent == null) {
                 return null;
             }
-            String formattedMessage = nullToEmpty(loggingEvent.getMessage());
-            Level level = loggingEvent.glowroot$getLevel();
-            int lvl = level == null ? 0 : level.intValue();
-            Throwable t = loggingEvent.getThrown();
-            if (LoggerPlugin.markTraceAsError(lvl >= ERROR_INT, lvl >= WARN_INT, t != null)) {
+            final String formattedMessage = formatter.format(loggingEvent);
+            final Level level = loggingEvent.getLevel(); // cannot be null
+            final int lvl = level.intValue();
+            final Throwable t = loggingEvent.getThrown();
+            if (LoggerPlugin.markTraceAsError(lvl >= Level.SEVERE.intValue(), lvl >= Level.WARNING.intValue(), t != null)) {
                 context.setTransactionError(formattedMessage, t);
             }
-            TraceEntry traceEntry;
-            String loggerName = LoggerPlugin.getAbbreviatedLoggerName(loggingEvent.getLoggerName());
-            traceEntry = context.startTraceEntry(MessageSupplier.create("log {}: {} - {}",
-                    getLevelStr(lvl), loggerName, formattedMessage), timerName);
+            final String loggerName = LoggerPlugin.getAbbreviatedLoggerName(loggingEvent.getLoggerName());
+            final TraceEntry traceEntry = context.startTraceEntry(MessageSupplier.create("log {}: {} - {}",
+                    level.getName(), loggerName, formattedMessage), timerName);
             return new LogAdviceTraveler(traceEntry, lvl, formattedMessage, t);
         }
 
@@ -98,50 +83,21 @@ public class JavaLoggingAspect {
             if (traveler == null) {
                 return;
             }
-            Throwable t = traveler.throwable;
+            final Throwable t = traveler.throwable;
             if (t != null) {
                 // intentionally not passing message since it is already the trace entry message
-                if (traveler.level >= WARN_INT) {
+                if (traveler.level >= Level.WARNING.intValue()) {
                     traveler.traceEntry.endWithError(t);
                 } else {
                     traveler.traceEntry.endWithInfo(t);
                 }
-            } else if (traveler.level >= WARN_INT) {
+            } else if (traveler.level >= Level.WARNING.intValue()) {
                 traveler.traceEntry.endWithError(traveler.formattedMessage);
             } else {
                 traveler.traceEntry.end();
             }
         }
-
-        private static String nullToEmpty(@Nullable String s) {
-            return s == null ? "" : s;
-        }
     }
-
-	private static String getLevelStr(int lvl) {
-		switch (lvl) {
-		case ALL_INT:
-			return "all";
-		case FINEST_INT:
-			return "finest";
-		case FINER_INT:
-			return "finer";
-		case FINE_INT:
-			return "fine";
-		case CONFIG_INT:
-			return "config";
-		case INFO_INT:
-			return "info";
-		case WARN_INT:
-			return "warn";
-		case ERROR_INT:
-			return "error";
-		case OFF_INT:
-			return "off";
-		default:
-			return "unknown (" + lvl + ")";
-		}
-	}
 
     private static class LogAdviceTraveler {
 
