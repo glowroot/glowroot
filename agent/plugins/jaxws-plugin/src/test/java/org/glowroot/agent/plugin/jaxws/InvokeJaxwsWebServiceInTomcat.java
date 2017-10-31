6b19ad2c87a163dd65b95f0a1ea7b59bb3e220a4
Trask Stalnaker <trask.stalnaker@gmail.com>
2017-10-31 13:19:33 -0700

    Better error message on test failure

--- agent/plugins/jaxws-plugin/src/test/java/org/glowroot/agent/plugin/jaxws/InvokeJaxwsWebServiceInTomcat.java
+++ agent/plugins/jaxws-plugin/src/test/java/org/glowroot/agent/plugin/jaxws/InvokeJaxwsWebServiceInTomcat.java
@@ -16,19 +16,26 @@
 package org.glowroot.agent.plugin.jaxws;
 
 import java.io.File;
+import java.lang.reflect.Method;
 import java.net.ServerSocket;
 
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
 import org.apache.catalina.Context;
+import org.apache.catalina.loader.WebappClassLoaderBase;
 import org.apache.catalina.loader.WebappLoader;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.glowroot.agent.it.harness.AppUnderTest;
 
 abstract class InvokeJaxwsWebServiceInTomcat implements AppUnderTest {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(InvokeJaxwsWebServiceInTomcat.class);
 
     public void executeApp(String webapp, String contextPath, String url) throws Exception {
         int port = getAvailablePort();
@@ -50,8 +57,37 @@ abstract class InvokeJaxwsWebServiceInTomcat implements AppUnderTest {
         ForBothHelloAndRootService client = (ForBothHelloAndRootService) factory.create();
         client.echo("abc");
 
+        checkForRequestThreads(webappLoader);
         tomcat.stop();
         tomcat.destroy();
+    }
+
+    // log stack traces for any currently running request threads in order to troubleshoot sporadic
+    // test failures due to ERROR logged by org.apache.catalina.loader.WebappClassLoaderBase
+    // "The web application [] is still processing a request that has yet to finish. This is very
+    // likely to create a memory leak..."
+    private void checkForRequestThreads(WebappLoader webappLoader) throws Exception {
+        Method getThreadsMethod = WebappClassLoaderBase.class.getDeclaredMethod("getThreads");
+        getThreadsMethod.setAccessible(true);
+        Method isRequestThreadMethod =
+                WebappClassLoaderBase.class.getDeclaredMethod("isRequestThread", Thread.class);
+        isRequestThreadMethod.setAccessible(true);
+        ClassLoader webappClassLoader = webappLoader.getClassLoader();
+        Thread[] threads = (Thread[]) getThreadsMethod.invoke(webappClassLoader);
+        for (Thread thread : threads) {
+            if (thread == null) {
+                continue;
+            }
+            if ((boolean) isRequestThreadMethod.invoke(webappClassLoader, thread)) {
+                StringBuilder sb = new StringBuilder();
+                for (StackTraceElement element : thread.getStackTrace()) {
+                    sb.append(element);
+                    sb.append('\n');
+                }
+                logger.error("tomcat request thread \"{}\" is still active:\n{}", thread.getName(),
+                        sb);
+            }
+        }
     }
 
     private static int getAvailablePort() throws Exception {
--- agent/plugins/spring-plugin/src/test/java/org/glowroot/agent/plugin/spring/InvokeSpringControllerInTomcat.java
+++ agent/plugins/spring-plugin/src/test/java/org/glowroot/agent/plugin/spring/InvokeSpringControllerInTomcat.java
@@ -16,16 +16,23 @@
 package org.glowroot.agent.plugin.spring;
 
 import java.io.File;
+import java.lang.reflect.Method;
 import java.net.ServerSocket;
 
 import com.ning.http.client.AsyncHttpClient;
 import org.apache.catalina.Context;
+import org.apache.catalina.loader.WebappClassLoaderBase;
 import org.apache.catalina.loader.WebappLoader;
 import org.apache.catalina.startup.Tomcat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.glowroot.agent.it.harness.AppUnderTest;
 
 abstract class InvokeSpringControllerInTomcat implements AppUnderTest {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(InvokeSpringControllerInTomcat.class);
 
     public void executeApp(String webapp, String contextPath, String url) throws Exception {
         int port = getAvailablePort();
@@ -40,6 +47,7 @@ abstract class InvokeSpringControllerInTomcat implements AppUnderTest {
         context.setLoader(webappLoader);
 
         tomcat.start();
+
         AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
         int statusCode = asyncHttpClient.prepareGet("http://localhost:" + port + contextPath + url)
                 .execute().get().getStatusCode();
@@ -47,12 +55,14 @@ abstract class InvokeSpringControllerInTomcat implements AppUnderTest {
         if (statusCode != 200) {
             throw new IllegalStateException("Unexpected status code: " + statusCode);
         }
+
         // spring still does a bit of work after the response is concluded,
         // see org.springframework.web.servlet.FrameworkServlet.publishRequestHandledEvent(),
         // so give a bit of time here, otherwise end up with sporadic test failures due to
         // ERROR logged by org.apache.catalina.loader.WebappClassLoaderBase, e.g.
         // "The web application [] is still processing a request that has yet to finish"
         Thread.sleep(200);
+        checkForRequestThreads(webappLoader);
         tomcat.stop();
         tomcat.destroy();
     }
@@ -62,5 +72,33 @@ abstract class InvokeSpringControllerInTomcat implements AppUnderTest {
         int port = serverSocket.getLocalPort();
         serverSocket.close();
         return port;
+    }
+
+    // log stack traces for any currently running request threads in order to troubleshoot sporadic
+    // test failures due to ERROR logged by org.apache.catalina.loader.WebappClassLoaderBase
+    // "The web application [] is still processing a request that has yet to finish. This is very
+    // likely to create a memory leak..."
+    private void checkForRequestThreads(WebappLoader webappLoader) throws Exception {
+        Method getThreadsMethod = WebappClassLoaderBase.class.getDeclaredMethod("getThreads");
+        getThreadsMethod.setAccessible(true);
+        Method isRequestThreadMethod =
+                WebappClassLoaderBase.class.getDeclaredMethod("isRequestThread", Thread.class);
+        isRequestThreadMethod.setAccessible(true);
+        ClassLoader webappClassLoader = webappLoader.getClassLoader();
+        Thread[] threads = (Thread[]) getThreadsMethod.invoke(webappClassLoader);
+        for (Thread thread : threads) {
+            if (thread == null) {
+                continue;
+            }
+            if ((boolean) isRequestThreadMethod.invoke(webappClassLoader, thread)) {
+                StringBuilder sb = new StringBuilder();
+                for (StackTraceElement element : thread.getStackTrace()) {
+                    sb.append(element);
+                    sb.append('\n');
+                }
+                logger.error("tomcat request thread \"{}\" is still active:\n{}", thread.getName(),
+                        sb);
+            }
+        }
     }
 }
