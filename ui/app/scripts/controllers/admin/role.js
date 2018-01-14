f8e0289331a3eb59824829badaed2be1923ba103
Trask Stalnaker <trask.stalnaker@gmail.com>
2018-01-14 13:29:17 -0800

    Add some help text and other minor UI improvement

--- ui/app/scripts/controllers/admin/role.js
+++ ui/app/scripts/controllers/admin/role.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2017 the original author or authors.
+ * Copyright 2016-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -80,6 +80,12 @@ glowroot.controller('AdminRoleCtrl', [
         populatePermissionBlock($scope.page.permissions, permission);
       });
       $scope.allActiveAgentRollups = data.allActiveAgentRollups;
+      $scope.rollupsExist = false;
+      angular.forEach($scope.allActiveAgentRollups, function (agentRollup) {
+        if ($scope.isRollup(agentRollup.id)) {
+          $scope.rollupsExist = true;
+        }
+      });
       angular.forEach($scope.allActiveAgentRollups, function (agentRollup) {
         var indent = '';
         for (var i = 0; i < agentRollup.depth; i++) {
