b0fe66987864aacbb90531be28110dc3988a7756
Trask Stalnaker <trask.stalnaker@gmail.com>
2018-01-16 21:51:27 -0800

    Fix slow threshold is capped by partial threshold

--- agent/core/src/main/java/org/glowroot/agent/init/ImmediateTraceStoreWatcher.java
+++ agent/core/src/main/java/org/glowroot/agent/init/ImmediateTraceStoreWatcher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2016 the original author or authors.
+ * Copyright 2011-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -114,8 +114,9 @@ class ImmediateTraceStoreWatcher extends ScheduledRunnable {
                     transactionPreviouslyCompleted = true;
                     return;
                 }
+            } else if (transactionCollector.shouldStoreSlow(transaction)) {
+                transactionCollector.storePartialTrace(transaction);
             }
-            transactionCollector.storePartialTrace(transaction);
         }
     }
 }
