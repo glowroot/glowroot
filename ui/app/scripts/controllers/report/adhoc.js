8f7a230a05c71943cd66cc3b1f8b14f8991a2f68
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-01-01 12:04:21 -0800

    Do not lose agent selection on changing dates

--- ui/app/scripts/controllers/report/adhoc.js
+++ ui/app/scripts/controllers/report/adhoc.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2018 the original author or authors.
+ * Copyright 2016-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -79,6 +79,9 @@ glowroot.controller('ReportAdhocCtrl', [
 
     if ($scope.layout.central) {
       $scope.$watchGroup(['report.fromDate', 'report.toDate'], function () {
+        if ($scope.report.fromDate > $scope.report.toDate) {
+          return;
+        }
         var query = {
           fromDate: moment($scope.report.fromDate).format('YYYYMMDD'),
           toDate: moment($scope.report.toDate).format('YYYYMMDD'),
