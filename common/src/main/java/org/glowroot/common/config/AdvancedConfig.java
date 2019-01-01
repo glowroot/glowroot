9bdcd1da01d9c1cb94961d4219ab88cd59899a2b
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-01-01 15:05:07 -0800

    Improve help text

--- common/src/main/java/org/glowroot/common/config/AdvancedConfig.java
+++ common/src/main/java/org/glowroot/common/config/AdvancedConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2018 the original author or authors.
+ * Copyright 2013-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,6 +38,7 @@ public abstract class AdvancedConfig {
     }
 
     // used to limit memory requirement
+    // (applied per transaction type)
     @Value.Default
     public int maxTransactionAggregates() {
         return ConfigDefaults.ADVANCED_MAX_TRANSACTION_AGGREGATES;
