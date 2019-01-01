07dbbcfe0ada386c473deb5e6d310fcf3dcdc125
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-01-01 13:36:30 -0800

    Fix alreadyInTransactionBehavior of "do-nothing"

--- agent/core/src/main/java/org/glowroot/agent/weaving/AdviceGenerator.java
+++ agent/core/src/main/java/org/glowroot/agent/weaving/AdviceGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2018 the original author or authors.
+ * Copyright 2013-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -268,6 +268,7 @@ class AdviceGenerator {
         visitAnnotation(mv, "Lorg/glowroot/agent/plugin/api/weaving/IsEnabled;");
         mv.visitCode();
         if (checkNotInTransaction && !checkPropertyNotEnabled) {
+            mv.visitVarInsn(ALOAD, 0);
             mv.visitMethodInsn(INVOKEINTERFACE,
                     "org/glowroot/agent/plugin/api/OptionalThreadContext", "isInTransaction", "()Z",
                     true);
@@ -286,6 +287,7 @@ class AdviceGenerator {
             mv.visitInsn(IRETURN);
         } else {
             // check both
+            mv.visitVarInsn(ALOAD, 0);
             mv.visitMethodInsn(INVOKEINTERFACE,
                     "org/glowroot/agent/plugin/api/OptionalThreadContext", "isInTransaction", "()Z",
                     true);
