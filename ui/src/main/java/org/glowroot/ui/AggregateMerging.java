f890f166f0779f0a833d8d044920560176655008
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-01-03 14:09:07 -0800

    Fix auxiliary breakdown not showing up

--- ui/src/main/java/org/glowroot/ui/AggregateMerging.java
+++ ui/src/main/java/org/glowroot/ui/AggregateMerging.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2018 the original author or authors.
+ * Copyright 2015-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,7 +57,7 @@ class AggregateMerging {
                 .mainThreadRootTimers(mainThreadRootTimers)
                 .mainThreadStats(mainThreadStats);
         if (auxThreadRootTimer.getCount() != 0) {
-            builder.auxThreadRootTimers(auxThreadRootTimer)
+            builder.auxThreadRootTimer(auxThreadRootTimer)
                     .auxThreadStats(auxThreadStats);
         }
         return builder.asyncTimers(asyncTimers)
@@ -91,7 +91,7 @@ class AggregateMerging {
         List<MutableTimer> mainThreadRootTimers();
         MutableThreadStats mainThreadStats();
         @Nullable
-        MutableTimer auxThreadRootTimers();
+        MutableTimer auxThreadRootTimer();
         @Nullable
         MutableThreadStats auxThreadStats();
         List<MutableTimer> asyncTimers();
