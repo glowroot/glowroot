d2d24a67bfc55e047a6fb74f0a7ca29d07bf30dc
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-01-13 19:48:32 -0800

    Support dynamically named timers
    See https://groups.google.com/d/msg/glowroot/siVsE0oj374/spGvLUMkDAAJ

--- agent/core/src/main/java/org/glowroot/agent/impl/TimerNameCache.java
+++ agent/core/src/main/java/org/glowroot/agent/impl/TimerNameCache.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2018 the original author or authors.
+ * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,7 +36,9 @@ public class TimerNameCache {
 
     private static final Logger logger = LoggerFactory.getLogger(TimerNameCache.class);
 
+    // weak values is to support dynamically named timers
     private final LoadingCache<String, TimerNameImpl> names = CacheBuilder.newBuilder()
+            .weakValues()
             .build(new CacheLoader<String, TimerNameImpl>() {
                 @Override
                 public TimerNameImpl load(String name) {
