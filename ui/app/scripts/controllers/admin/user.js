c41cf866e4ce900a4cc958ccc7e07678cfa9cb38
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-01-17 17:09:45 -0800

    Minor UI improvement

--- ui/app/scripts/controllers/admin/user.js
+++ ui/app/scripts/controllers/admin/user.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2017 the original author or authors.
+ * Copyright 2016-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -104,6 +104,13 @@ glowroot.controller('AdminUserCtrl', [
             httpErrors.handle(response, $scope);
           });
     }
+
+    $scope.$watch('config.ldap', function () {
+      if ($scope.config && $scope.config.ldap) {
+        delete $scope.page.password;
+        delete $scope.page.verifyPassword;
+      }
+    });
 
     $scope.$watch('allRoles', function (newValue, oldValue) {
       if (newValue !== oldValue) {
