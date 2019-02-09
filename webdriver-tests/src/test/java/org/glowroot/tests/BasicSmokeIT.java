a2ea0f78df04d1d35100cd4b329cd85e9ddbb1c4
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-02-09 14:52:06 -0800

    Remove "experimental" text from flame graph

--- webdriver-tests/src/test/java/org/glowroot/tests/BasicSmokeIT.java
+++ webdriver-tests/src/test/java/org/glowroot/tests/BasicSmokeIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2018 the original author or authors.
+ * Copyright 2014-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -452,7 +452,7 @@ public class BasicSmokeIT extends WebDriverIT {
         click(xpath("//button[@ng-click='refresh()']"));
         new WebDriverWait(driver, 30).until(ExpectedConditions
                 .textToBePresentInElementLocated(className("gt-profile"), "JdbcServlet"));
-        clickLink("View flame graph (experimental)");
+        clickLink("View flame graph");
         // give flame graph a chance to render (only for visual when running locally)
         SECONDS.sleep(1);
     }
