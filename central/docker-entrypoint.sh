4a4a8e3aebb7194fba2515c163d5d83458b1ddbd
Sylvere Richard <sylvere.richard@gmail.com>
2019-07-05 18:56:52 +0200

    Add support for docker secrets
    Also add UI_CONTEXT_PATH to set context path from an env variable.

--- central/docker-entrypoint.sh
+++ central/docker-entrypoint.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 set -e
 
+# file_env function is same as from
+# https://github.com/docker-library/postgres/blob/master/11/docker-entrypoint.sh
+# and https://github.com/docker-library/mariadb/blob/master/10.4/docker-entrypoint.sh
+#
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+  local var="$1"
+  local fileVar="${var}_FILE"
+  local def="${2:-}"
+  if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+    echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+    exit 1
+  fi
+  local val="$def"
+  if [ "${!var:-}" ]; then
+    val="${!var}"
+  elif [ "${!fileVar:-}" ]; then
+    val="$(< "${!fileVar}")"
+  fi
+  export "$var"="$val"
+  unset "$fileVar"
+}
+
+file_env 'CASSANDRA_PASSWORD'
+file_env 'CASSANDRA_SYMMETRIC_ENCRYPTION_KEY'
+
 if [ "$CASSANDRA_CONTACT_POINTS" ]; then
   sed -i "s/^cassandra.contactPoints=.*$/cassandra.contactPoints=$CASSANDRA_CONTACT_POINTS/" glowroot-central.properties
 fi
@@ -18,6 +47,9 @@ if [ "$CASSANDRA_CONSISTENCY_LEVEL" ]; then
 fi
 if [ "$CASSANDRA_SYMMETRIC_ENCRYPTION_KEY" ]; then
   sed -i "s/^cassandra.symmetricEncryptionKey=.*$/cassandra.symmetricEncryptionKey=$CASSANDRA_SYMMETRIC_ENCRYPTION_KEY/" glowroot-central.properties
+fi
+if [ "$UI_CONTEXT_PATH" ]; then
+  sed -i "s/^ui.contextPath=.*$/ui.contextPath=$UI_CONTEXT_PATH/" glowroot-central.properties
 fi
 
 exec "$@"
