590200ccf7adb5589cc2df2227d117955e51d3db
Sachin Mittal <sjmittal@gmail.com>
2020-02-21 10:24:42 +0530

    fix on flush of collector
    check if active collector has any transactions

--- agent/core/src/main/java/org/glowroot/agent/impl/TransactionProcessor.java
+++ agent/core/src/main/java/org/glowroot/agent/impl/TransactionProcessor.java
@@ -258,7 +258,9 @@ public class TransactionProcessor {
         }
 
         private void flushAndResetActiveIntervalCollector(long currentTime) {
-            flushActiveIntervalCollector();
+            if (activeIntervalCollector.size() > 0) {
+                flushActiveIntervalCollector();
+            }
             activeIntervalCollector =
                     new AggregateIntervalCollector(currentTime, aggregateIntervalMillis,
                             configService.getAdvancedConfig().maxTransactionAggregates(),
