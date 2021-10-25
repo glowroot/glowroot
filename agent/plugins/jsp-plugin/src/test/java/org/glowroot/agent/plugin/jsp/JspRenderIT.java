2e5a82d12aa0d017e15a74f2fbec58730052f224
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-10-24 18:44:44 -0700

    fix jsp

--- agent/plugins/jsp-plugin/src/test/java/org/glowroot/agent/plugin/jsp/JspRenderIT.java
+++ agent/plugins/jsp-plugin/src/test/java/org/glowroot/agent/plugin/jsp/JspRenderIT.java
@@ -29,7 +29,6 @@ import com.ning.http.client.AsyncHttpClient;
 import org.apache.catalina.Context;
 import org.apache.catalina.loader.WebappLoader;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.TomcatURLStreamHandlerFactory;
 import org.apache.jasper.servlet.JspServlet;
 import org.apache.jsp.WEB_002dINF.jsp.index_jsp;
 import org.junit.After;
@@ -113,9 +112,6 @@ public class JspRenderIT {
 
         @Override
         public void executeApp() throws Exception {
-            // otherwise tests can fail with "factory already defined"
-            TomcatURLStreamHandlerFactory.disable();
-
             int port = getAvailablePort();
             Tomcat tomcat = new Tomcat();
             tomcat.setBaseDir("target/tomcat");
