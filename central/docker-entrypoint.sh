045a1257a54c6b7dec19909de66f32e2ea94f2bf
Sylvere Richard <Sylvere.Richard@gmail.com>
2022-04-02 01:01:16 +0200

    Use jdk 17 for glowroot central (#909)
    * migration to infinispan 13
    
    * grpc migration
    
    * log version update
    
    * replace sasl with symmetric encryption
    
    sasl from jgroups 4.x does not work with jdk 17 and is removed from jgroups 5.x
    
    * Use jdk 17 for glowroot-central
    
    * jgroups config with docker
    
    * Update central/Dockerfile
    
    * Update central/Dockerfile
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

--- central/docker-entrypoint.sh
+++ central/docker-entrypoint.sh
@@ -29,6 +29,8 @@ file_env() {
 
 file_env 'CASSANDRA_PASSWORD'
 file_env 'CASSANDRA_SYMMETRIC_ENCRYPTION_KEY'
+file_env 'JGROUPS_SYM_ENCRYPT_KEYSTORE_PASSWORD'
+file_env 'JGROUPS_SYM_ENCRYPT_KEY_PASSWORD'
 
 if [ "$CASSANDRA_CONTACT_POINTS" ]; then
   sed -i "s/^cassandra.contactPoints=.*$/cassandra.contactPoints=$CASSANDRA_CONTACT_POINTS/" glowroot-central.properties
@@ -51,6 +53,33 @@ fi
 if [ "$UI_CONTEXT_PATH" ]; then
   # not using "/" as sed delimiter since it is a common character used in context path
   sed -i "s|^ui.contextPath=.*$|ui.contextPath=$UI_CONTEXT_PATH|" glowroot-central.properties
+fi
+if [ "$JGROUPS_CONFIGURATION_FILE" ]; then
+  sed -i "s/^jgroups.configurationFile=.*$/jgroups.configurationFile=$JGROUPS_CONFIGURATION_FILE/" glowroot-central.properties
+fi
+if [ "$JGROUPS_LOCAL_ADDRESS" ]; then
+  sed -i "s/^jgroups.localAddress=.*$/jgroups.localAddress=$JGROUPS_LOCAL_ADDRESS/" glowroot-central.properties
+fi
+if [ "$JGROUPS_LOCAL_PORT" ]; then
+  sed -i "s/^jgroups.localPort=.*$/jgroups.localPort=$JGROUPS_LOCAL_PORT/" glowroot-central.properties
+fi
+if [ "$JGROUPS_INITIAL_NODES" ]; then
+  sed -i "s/^jgroups.initialNodes=.*$/jgroups.initialNodes=$JGROUPS_INITIAL_NODES/" glowroot-central.properties
+fi
+if [ "$JGROUPS_SYM_ENCRYPT_ALGORITHM" ]; then
+  sed -i "s|^jgroups.symEncryptAlgorithm=.*$|jgroups.symEncryptAlgorithm=$JGROUPS_SYM_ENCRYPT_ALGORITHM|" glowroot-central.properties
+fi
+if [ "$JGROUPS_SYM_ENCRYPT_KEYSTORE_LOCATION" ]; then
+  sed -i "s|^jgroups.symEncryptKeystoreLocation=.*$|jgroups.symEncryptKeystoreLocation=$JGROUPS_SYM_ENCRYPT_KEYSTORE_LOCATION|" glowroot-central.properties
+fi
+if [ "$JGROUPS_SYM_ENCRYPT_KEYSTORE_PASSWORD" ]; then
+  sed -i "s/^jgroups.symEncryptKeystorePassword=.*$/jgroups.symEncryptKeystorePassword=$JGROUPS_SYM_ENCRYPT_KEYSTORE_PASSWORD/" glowroot-central.properties
+fi
+if [ "$JGROUPS_SYM_ENCRYPT_KEY_ALIAS" ]; then
+  sed -i "s/^jgroups.symEncryptKeyAlias=.*$/jgroups.symEncryptKeyAlias=$JGROUPS_SYM_ENCRYPT_KEY_ALIAS/" glowroot-central.properties
+fi
+if [ "$JGROUPS_SYM_ENCRYPT_KEY_PASSWORD" ]; then
+  sed -i "s/^jgroups.symEncryptKeyPassword=.*$/jgroups.symEncryptKeyPassword=$JGROUPS_SYM_ENCRYPT_KEY_PASSWORD/" glowroot-central.properties
 fi
 
 exec "$@"
